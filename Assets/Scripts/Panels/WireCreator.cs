249eac136bf2a0e2e4e053f680695a7dbe669ca7
hunterdyar <bloop@hdyar.com>
2024-09-04 21:47:44 -0400

    font

---
+++ Assets/Scripts/Panels/WireCreator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Zoompy.Panels
+{
+	public class WireCreator : MonoBehaviour
+	{
+		//Generate Connections
+		//Connection -> 90turn to height of other connection, 90turn, connect. 
+		
+		//1. Figure out how to create knots that do the correct radius
+		//2. Figure out how to decide where to place these knots to make it good. 
+	}
+}
